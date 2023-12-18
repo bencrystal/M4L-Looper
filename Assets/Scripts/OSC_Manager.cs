7d93d8fabfd175718d1510d4282840716b4ef8a1
bencrystal <46793135+bencrystal@users.noreply.github.com>
2023-12-18 05:22:22 -0500

    Added controls for second looper
    Minimally functioning in editor with mouse controls.  Next step is trying to figure out how to get it working over IP from Quest to Mac

--- Assets/Scripts/OSC_Manager.cs
+++ Assets/Scripts/OSC_Manager.cs
@@ -22,6 +22,22 @@ public class OSC_Manager : MonoBehaviour
     [SerializeField]
     private Scrollbar ScrollbarL1V;
 
+
+    [SerializeField] 
+    private Button ButtonL2S;
+
+    [SerializeField] 
+    private Button ButtonL2R;
+
+    [SerializeField] 
+    private Button ButtonL2P;
+
+    [SerializeField] 
+    private Button ButtonL2O;
+
+    [SerializeField]
+    private Scrollbar ScrollbarL2V;
+
     //need to initialize empty variables to be sent as messages(?)
     /*private object stop;
     private object play;
@@ -39,7 +55,13 @@ public class OSC_Manager : MonoBehaviour
         ButtonL1R.onClick.AddListener(() => OnButtonPressed(ButtonL1R));
         ButtonL1P.onClick.AddListener(() => OnButtonPressed(ButtonL1P));
         ButtonL1O.onClick.AddListener(() => OnButtonPressed(ButtonL1O));
+        ScrollbarL1V.onValueChanged.AddListener((val) => OnValueChanged(ScrollbarL1V, val));
 
+        ButtonL2S.onClick.AddListener(() => OnButtonPressed(ButtonL2S));
+        ButtonL2R.onClick.AddListener(() => OnButtonPressed(ButtonL2R));
+        ButtonL2P.onClick.AddListener(() => OnButtonPressed(ButtonL2P));
+        ButtonL2O.onClick.AddListener(() => OnButtonPressed(ButtonL2O));
+        ScrollbarL2V.onValueChanged.AddListener((val) => OnValueChanged(ScrollbarL2V, val));
         //osc.SetAddressHandler("/looper1", OnButtonPressed);
 
        
@@ -88,6 +110,32 @@ public class OSC_Manager : MonoBehaviour
                 //message.values.Add(overdub);
                 break;
 
+                case "Button_L2S":
+                message.address = "/looper2";
+                message.values.Add(0);
+                //message.values.Add(stop);
+                //message.values.Add(1);
+                Debug.Log("ButtonL1S!!!");
+                break;
+
+                case "Button_L2R":
+                message.address = "/looper2";
+                message.values.Add(1);
+                //message.values.Add(record);
+                break;
+
+                case "Button_L2P":
+                message.address = "/looper2";
+                message.values.Add(2);
+                //message.values.Add(play);
+                break;
+
+                case "Button_L2O":
+                message.address = "/looper2";
+                message.values.Add(3);
+                //message.values.Add(overdub);
+                break;
+
                 default:
                 break;
             }
@@ -104,7 +152,68 @@ public class OSC_Manager : MonoBehaviour
 
         }
 
+
+
+
+    public void OnValueChanged(Scrollbar scrollbar, float val) //OscMessage message)
+        {
+
+            OscMessage message = new OscMessage();
+
+            //message.address = "/looper1";
+            Debug.Log(scrollbar.name);
+            //underscores are necessary for distinguishing buttons in scene from script references
+            //osc.add.values seem to be of a type that can take strings
+            switch(scrollbar.name)
+            {
+                case "Scrollbar_L1V":
+                message.address = "/looper1";
+                message.values.Add(val); //make sure to clamp from 0-.85(?), and this way 0 will also stop the track?
+                //message.values.Add(stop);
+                //message.values.Add(1);
+                Debug.Log("Scrollbar L1V was " + val);
+                break;
+    
+                case "Scrollbar_L2V":
+                message.address = "/looper2";
+                message.values.Add(val);
+                //message.values.Add(record);
+                Debug.Log("Scrollbar L2V was " + val);
+                break;
+        /*
+                case "Scrollbar_L3V":
+                message.address = "/looper3";
+                message.values.Add(val);
+                //message.values.Add(play);
+                Debug.Log("Scrollbar L3V was " + val);
+                break;
+
+                case "Scrollbar_L4V":
+                message.address = "/looper4";
+                message.values.Add(val);
+                //message.values.Add(overdub);
+                Debug.Log("Scrollbar L4V was " + val);
+                break;
+    */
+                default:
+                break;
+            }
+            
+            //message.values.Add(0);
+
+            osc.Send(message);
+
+            //Debug.Log(buttonPressed + " was pressed");
+
+            //send message to Ableton
+            
+
+
+        }
+
 }
+
+
 
 
 
